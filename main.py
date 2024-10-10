be387fc3f656ce1bad69dad17e58ef2fd2cb64ed
Chani17 <cucute8878@naver.com>
2024-10-10 21:22:09 +0900

    Feat: Return .wav

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from typing import Union
-
-from fastapi import FastAPI
+import io
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+from TTS.api import TTS
 
 app = FastAPI()
 
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class InputSentence(BaseModel):
+    sentence: str
+    speaker: str
+    language: str
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/generate")
+async def generate_speech(input: InputSentence):
+    try:
+        audio_buffer = io.BytesIO()
+        tts.tts_to_file(text=input.sentence, speaker=input.speaker, language=input.language, file_path=audio_buffer)
+        audio_buffer.seek(0)
+
+        return StreamingResponse(audio_buffer, media_type="audio/wav")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=7773)
