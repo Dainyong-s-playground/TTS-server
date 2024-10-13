6de8fd8ad30e46bbc97fee838993c073b1068f43
Chani17 <cucute8878@naver.com>
2024-10-14 00:06:36 +0900

    Update:  Replaced the Coqui model with the gTTS model

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import io
-import torch
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
-from TTS.api import TTS
+from gtts import gTTS
 
 app = FastAPI()
 
-tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2")
-
 class InputSentence(BaseModel):
     sentence: str
-    speaker: str = "XTTS_v2/ko-KR-6"
     language: str = "ko"
 
 @app.post("/generate")
 async def generate_speech(input: InputSentence):
     try:
         audio_buffer = io.BytesIO()
-        tts.tts_to_file(text=input.sentence, speaker=input.speaker, language=input.language, file_path=audio_buffer)
+        tts = gTTS(text=input.sentence, lang=input.language)
+        tts.write_to_fp(audio_buffer)
         audio_buffer.seek(0)
 
-        return StreamingResponse(audio_buffer, media_type="audio/wav")
+        return StreamingResponse(audio_buffer, media_type="audio/mp3")
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
